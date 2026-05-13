845877a679eed61d749bb68310f3b6a010407436
NyunuBit <nyunu@proton.me>
2026-05-13 10:36:46 -0300

    forgot to add the gap transition to not leave unecessary padding back in

--- src/lib/components/areas/navbar.svelte
+++ src/lib/components/areas/navbar.svelte
@@ -9,7 +9,10 @@
 	>
 		<NavBarButton path="/">
 			{#snippet children()}
-				<a href="/" class="flex gap-2">
+				<a
+					href="/"
+					class="flex transition-[gap] gap-0 group-hover:gap-2"
+				>
 					<House></House>
 					<p
 						class="size-full duration-250 group-hover:w-auto group-hover:opacity-100 w-0 opacity-0"
@@ -21,7 +24,10 @@
 		</NavBarButton>
 		<NavBarButton path="/projects">
 			{#snippet children()}
-				<a href="/projects" class="flex gap-2">
+				<a
+					href="/projects"
+					class="flex transition-[gap] gap-0 group-hover:gap-2"
+				>
 					<Rocket></Rocket>
 					<p
 						class="size-full duration-250 group-hover:w-auto group-hover:opacity-100 w-0 opacity-0"
@@ -33,7 +39,10 @@
 		</NavBarButton>
 		<NavBarButton path="/comms">
 			{#snippet children()}
-				<a href="/comms" class="flex gap-2">
+				<a
+					href="/comms"
+					class="flex transition-[gap] gap-0 group-hover:gap-2"
+				>
 					<Store></Store>
 					<p
 						class="size-full duration-250 group-hover:w-auto group-hover:opacity-100 w-0 opacity-0"
@@ -45,7 +54,10 @@
 		</NavBarButton>
 		<NavBarButton path="/contact">
 			{#snippet children()}
-				<a href="/contact" class="flex gap-2">
+				<a
+					href="/contact"
+					class="flex transition-[gap] gap-0 group-hover:gap-2"
+				>
 					<Phone></Phone>
 					<p
 						class="size-full duration-250 group-hover:w-auto group-hover:opacity-100 w-0 opacity-0"
