1c5e830f06c904676b6a68ff1dd14dc9c5a9d67c
NyunuBit <nyunu@proton.me>
2026-05-13 08:15:27 -0300

    simple sticky footer implementation

---
+++ src/lib/components/areas/footer.svelte
@@ -0,0 +1,7 @@
+<div
+	class="bottom-0 w-full flex fixed p-2 justify-between items-center bg-background"
+>
+	<br />
+	<br />
+	<p class="text-text/50">By Nyunu</p>
+</div>
--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -2,6 +2,7 @@
 	import "./layout.css";
 	import favicon from "$lib/assets/favicon.svg";
 	import Header from "$lib/components/areas/header.svelte";
+	import Footer from "$lib/components/areas/footer.svelte";
 	let { children } = $props();
 </script>
 
@@ -9,3 +10,4 @@
 
 <Header></Header>
 {@render children()}
+<Footer></Footer>
