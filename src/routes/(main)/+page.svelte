ee2706b1e222467b7494c78bc15bfef687f6477d
NyunuBit <nyunu@proton.me>
2026-05-12 23:32:15 -0300

    Basic "HUB" layout done

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,1 +1,17 @@
-<p>hello world</p>
+<script lang="ts">
+	import Logo from "$lib/assets/logo.svg";
+</script>
+
+<!-- main -->
+<div class="w-screen h-screen flex p-2 justify-center items-center">
+	<div class="justify-between flex flex-col gap-2 md:flex-row items-center">
+		<img src={Logo} alt="" width="512" height="512" />
+		<div class="max-w-lg h-full text-center md:text-left">
+			<h1 class="font-pixel-header text-9xl">NYUNU</h1>
+			<p>
+				Hi, i'm nyunu, i'm someone who really likes creating stuff on my
+				computer, and my computer.
+			</p>
+		</div>
+	</div>
+</div>
