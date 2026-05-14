a8e54e166654f206ff5ff57f777aa7099831f99b
NyunuBit <nyunu@proton.me>
2026-05-14 00:02:35 -0300

    abstracted navbar buttons

--- src/lib/components/areas/navbar.svelte
+++ src/lib/components/areas/navbar.svelte
@@ -1,66 +1,61 @@
 <script lang="ts">
 	import NavBarButton from "$lib/components/elements/navbar_btn.svelte";
-	import { House, Rocket, Store, Phone } from "@lucide/svelte";
+	import {
+		House,
+		Rocket,
+		Store,
+		Phone,
+		type LucideProps,
+	} from "@lucide/svelte";
+	import type { Component } from "svelte";
+
+	let buttons: {
+		name: string;
+		path: string;
+		icon: Component<LucideProps>;
+	}[] = [
+		{
+			path: "Home",
+			name: "/",
+			icon: House,
+		},
+		{
+			path: "Projects",
+			name: "/projects",
+			icon: Rocket,
+		},
+		{
+			path: "Commisions",
+			name: "/comms",
+			icon: Store,
+		},
+		{
+			path: "Contact",
+			name: "/contact",
+			icon: Phone,
+		},
+	];
 </script>
 
 <ul
 	class="overflow-hidden z-40 hidden md:flex flex-col group rounded-default-round duration-250 shadow-xl/30 bg-background"
 >
-	<NavBarButton path="/">
-		{#snippet children()}
-			<a href="/" class="flex transition-[gap] gap-0 group-hover:gap-2">
-				<House></House>
-				<p
-					class="size-full duration-250 group-hover:w-auto group-hover:opacity-100 w-0 opacity-0"
+	{#each buttons as button}
+		{@const Icon = button.icon}
+		<NavBarButton path={button.path}>
+			{#snippet children()}
+				<a
+					href="/"
+					class="flex transition-[gap] gap-0 group-hover:gap-2"
 				>
-					Home
-				</p>
-			</a>
-		{/snippet}
-	</NavBarButton>
-	<NavBarButton path="/projects">
-		{#snippet children()}
-			<a
-				href="/projects"
-				class="flex transition-[gap] gap-0 group-hover:gap-2"
-			>
-				<Rocket></Rocket>
-				<p
-					class="size-full duration-250 group-hover:w-auto group-hover:opacity-100 w-0 opacity-0"
-				>
-					Projects
-				</p>
-			</a>
-		{/snippet}
-	</NavBarButton>
-	<NavBarButton path="/comms">
-		{#snippet children()}
-			<a
-				href="/comms"
-				class="flex transition-[gap] gap-0 group-hover:gap-2"
-			>
-				<Store></Store>
-				<p
-					class="size-full duration-250 group-hover:w-auto group-hover:opacity-100 w-0 opacity-0"
-				>
-					Commisions
-				</p>
-			</a>
-		{/snippet}
-	</NavBarButton>
-	<NavBarButton path="/contact">
-		{#snippet children()}
-			<a
-				href="/contact"
-				class="flex transition-[gap] gap-0 group-hover:gap-2"
-			>
-				<Phone></Phone>
-				<p
-					class="size-full duration-250 group-hover:w-auto group-hover:opacity-100 w-0 opacity-0"
-				>
-					Contact
-				</p>
-			</a>
-		{/snippet}
-	</NavBarButton>
+					<Icon></Icon>
+					<p
+						class="size-full duration-250 group-hover:w-auto group-hover:opacity-100 w-0 opacity-0"
+					>
+						{button.name}
+					</p>
+				</a>
+			{/snippet}
+		</NavBarButton>
+	{/each}
 </ul>
