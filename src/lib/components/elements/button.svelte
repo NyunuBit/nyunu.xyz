d5ce883c7b10d53e77022a985d1870f6da26d8a5
NyunuBit <nyunu@proton.me>
2026-05-15 02:27:34 -0300

    button

---
+++ src/lib/components/elements/button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let {
+		children,
+		style,
+		onclick,
+		class: class_var,
+	}: {
+		children: any;
+		style?: string;
+		onclick?: () => void;
+		class?: string;
+	} = $props();
+
+	let styles: { [key: string]: string } = {
+		default: "bg-primary hover:bg-primary/90",
+	};
+</script>
+
+<button
+	{onclick}
+	class="{style == null
+		? styles.default
+		: styles[style]} {class_var} rounded-default-round cursor-pointer p-1"
+>
+	{@render children()}
+</button>
