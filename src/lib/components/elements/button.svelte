e71830684abbf140d2b64c0276467f421ae75a90
NyunuBit <nyunu@proton.me>
2026-05-19 00:08:34 -0300

    padding

--- src/lib/components/elements/button.svelte
+++ src/lib/components/elements/button.svelte
@@ -23,7 +23,7 @@
 		? styles.default
 		: styles[
 				style
-			]} {class_var} flex gap-2 rounded-default-round cursor-pointer p-1"
+			]} {class_var} flex gap-2 rounded-default-round cursor-pointer p-2"
 >
 	{@render children?.()}
 </button>
