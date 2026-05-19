67ddcd0768dbc64655151b8b3eeb7cc5f8283de2
NyunuBit <nyunu@proton.me>
2026-05-19 01:01:15 -0300

    projects page

---
+++ src/routes/(main)/projects/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<main class="md:m-20 m-2 grow">
+	<h1
+		class="mb-2 font-pixel-header text-3xl font-bold md:mx-10 md:w-auto w-full md:text-left text-center"
+	>
+		Projects
+	</h1>
+	<section
+		class="p-2 grid gap-2 md:grid-cols-4 grid-cols-1 bg-black/20 border border-text/5 rounded-b-default-round"
+	>
+		<button
+			class="rounded-default-round bg-background border p-2 aspect-square flex flex-col cursor-pointer items-stretch text-left border-text/5 hover:border-text/15"
+		>
+			<div
+				style="background-image: url('https://as2.ftcdn.net/jpg/02/18/72/73/1000_F_218727336_yFWOEXi4dqc01tCWSNZg2JSa0TPHtz4h.webp');"
+				class="w-full aspect-video bg-cover bg-no-repeat mb-3"
+				role="img"
+				aria-label="title"
+			></div>
+			<p class="text-primary italic">Type</p>
+			<h1 class="font-pixel-header font-bold text-4xl">Lorum Ipsum</h1>
+			<p class="text-text/70 text-[18px]">
+				Quick description, short short
+			</p>
+		</button>
+	</section>
+</main>
